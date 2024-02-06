cb1dba7f0e0b8039e33ba3b8080de42ce33e4f58
Cart-stack <salamitunde43@gmail.com>
2024-02-06 13:26:29 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+   <div className="App">
+    <div className="content">
+    <h1>Cart's Homepage</h1>
+
+    <Navbar />
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+const home = () => {
+
+
+
+    return ( 
+      <div className="home">
+       <h2>HomePage</h2>
+      </div> 
+     );
+}
+ 
+export default home ;
